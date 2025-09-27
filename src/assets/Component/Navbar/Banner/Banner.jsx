50b6768c3098ecb7cd679b25d7e19ea35422a3f2
mdmahfuzahmedony <Mdmahfuzahmedony@gmail.com>
2025-09-27 19:18:34 +0600

    fixed some problem

--- src/assets/Component/Navbar/Banner/Banner.jsx
+++ src/assets/Component/Navbar/Banner/Banner.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import inProgrss from "../../../../assets/vector1.png"
 const Banner = ({ Count , complateCount }) => {
   return (
     <div className=" max-w-[1300px] mx-auto my-20 flex flex-col md:flex-row justify-between items-center gap-7">
       <div className="rounded-[10px]  w-full md:flex-1 text-center bg-[#422AD5]  relative h-full">
         <img
           className="absolute w-[50%] h-full"
-          src="/B12A7-Customer-Support-Zone/vector1.png"
+          src={inProgrss}
           alt=""
         />
         <img
           className="absolute w-[50%] h-full right-0 scale-x-[-1]"
-          src="/B12A7-Customer-Support-Zone/vector1.png"
+          src={inProgrss}
           alt=""
         />
         <div className="py-12">
@@ -21,12 +22,12 @@ const Banner = ({ Count , complateCount }) => {
       <div className=" rounded-[10px] w-full md:flex-1 text-center bg-gradient-to-r from-green-400 to-emerald-600  relative h-full">
         <img
           className="absolute w-[50%] h-full"
-          src="/B12A7-Customer-Support-Zone/vector1.png"
+          src={inProgrss}
           alt=""
         />
         <img
           className="absolute w-[50%] h-full right-0 scale-x-[-1]"
-          src="/B12A7-Customer-Support-Zone/vector1.png"
+          src={inProgrss}
           alt=""
         />
         <div className="py-12">
